6654052b7dd1d1ae0d6aa4c84c659ee932356735
Nac-H <arturomechan20@gmail.com>
2025-10-06 20:55:16 -0500

    Delete Frontend directory

--- Frontend/src/API/datasource/index.ts
+++
@@ -1,7 +0,0 @@
-import LocalStorageDS from '../impl/ds/LocalStorageDS'
-import DataRepoImpl from '../impl/repo/DataRepoImpl'
-
-const LS = new LocalStorageDS()
-export const DataRepo = new DataRepoImpl(LS)
-
-
--- Frontend/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router'
-
-// Redirige automáticamente al Login
-export const Route = createFileRoute('/')({
-  component: () => <Navigate to="/Login/SIgn" replace />,
-})
