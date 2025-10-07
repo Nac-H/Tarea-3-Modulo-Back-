7aa5880565f41f4580caeed2ce8e5f68e6ef64e2
Nac-H <arturomechan20@gmail.com>
2025-10-06 20:55:24 -0500

    Delete Backend directory

--- Backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Conectado a la base de datos MongoDB'))
-    .catch(err => console.error('Error de conexión a MongoDB:', err));
-
-module.exports = mongoose;
